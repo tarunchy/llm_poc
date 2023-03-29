68aa383f4f49ff4e48aeec63173217907f7d8f32
dlyog <dlyog@dlyog02.lan>
2023-03-29 16:44:49 -0700

    Added

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export FLASK_APP=app.py
 export FLASK_ENV=development
-gunicorn --bind dlyog02:8000 app:app --pid app_id --timeout 0
+#gunicorn --bind dlyog02:8000 app:app --pid app_id --timeout 0
+gunicorn --bind dlyog02:8000 app:app --pid app_id --timeout 0 > model.log 2>&1 &
 
